3dc8110d0a648e84a7c1f60e9ed15484d6c98dd3
Caroline <wanguic49@gmail.com>
2025-01-18 11:23:51 +0300

    added a few fields

--- src/app/models/airbnb.ts
+++ src/app/models/airbnb.ts
@@ -1,7 +1,29 @@
 export interface Bnb {
     id: number;
+    name: string;
+    summary: string;
+    accomodates: number;
     bed_type: string;
     minimum_nights: string;
     cancellation_policy: string;
+    price: number;
+    images:{
+        thumbnail_url: string;
+        medium_url: string;
+        picture_url: string;
+    },
+    host:{
+        host_id: number;
+        host_url: string;
+        host_name: string;
+        host_thumbnail_url: string;
+    },
+    address:{
+        street: string;
+        city: string;
+        state: string;
+        country: string;
+        market: string;
+    },
   }
   
